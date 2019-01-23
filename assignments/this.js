7f80cb0c4b331ee8f0e20a2a25e1c063e5367a32
Clark <rclark010@gmail.com>
2019-01-23 13:25:43 -0700

    Completed the assignment in this.js

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,49 @@
-/* The for principles of "this";
-* in your own words. explain the four principle for the "this" keyword below.
+/* The four principles of "this";
+*  Explain in your own words the four principles of the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. When in the global scope, `this` refers to the `window`, which is the highest level in the browser. I believe you can think of this as the "default scope" of `this`.
+* 2. When a function is called using a preceeding dot (e.g. `person.speak()`), `this` refers to the object to the left of the dot. By using dot notation, you're implicity using a different "scope" for `this`.
+* 3. When creating an object with the `new` keyword, the `this` refers to the object being created.
+* 4. In contrast to implicitly binding `this`, you can explicitly bind `this` using JavaScript's `.call()`, `.apply()`, or `.bind()`. In that case, you actually pass to one of those functions the "scope" you want `this` to have.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
-
 // code example for Window Binding
 
-// Principle 2
+// Within Global Scope
+console.log(this); // will return the `window` object
 
+// Principle 2
 // code example for Implicit Binding
 
-// Principle 3
+const newObject = {
+    plantName: 'fern',
+    region: 'jungle',
+    funFact: function() {
+        console.log(`The ${this.plantName} can be found in the ${this.region}!`);
+    }
+}
+newObject.funFact();
 
+// Principle 3
 // code example for New Binding
 
-// Principle 4
+function PlantGenerator(arguments) {
+    this.plantName = arguments.plantName;
+    this.region = arguments.region;
+    this.herbalApplications = arguments.herbalApplications;
+}
 
+const eucalyptus = new PlantGenerator({
+    plantName: 'eucalyptus',
+    region: 'Australia',
+    herbalApplications: 'Soothes joint pain'
+});
+console.log(eucalyptus.herbalApplications);
+
+// Principle 4
 // code example for Explicit Binding
+
+newObject.funFact.call(eucalyptus) // hijacks newObject
